d651334dd84b16ed633e058f7c5ba35cbf6848d9
gautamchinku@gmail.com <gautamchinku@gmail.com>
2023-08-07 18:12:15 +0000

    commit

--- formula1_project/ingestion/1.ingest_circuit_files.py
+++ formula1_project/ingestion/1.ingest_circuit_files.py
@@ -78,9 +78,97 @@ circuits_df1.printSchema()
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC #### - This is about file system magic commands . calling dbutils.fs.mounts() is same as using magic commands 
+# MAGIC - Select only requiredColumns
 
 # COMMAND ----------
 
-# MAGIC %fs
-# MAGIC ls /mnt/formula1dlgo/raw
+## 1st way to selecting the dataframe 
+circuits_selected_df1 = circuits_df1.select("circuitId","circuitRef","name","location","country","lat","lng","alt")
+
+# COMMAND ----------
+
+display(circuits_selected_df1)
+
+# COMMAND ----------
+
+# 2nd way to selecting the dataframe 
+circuits_selected_df2 = circuits_df1.select(circuits_df1.circuitId,circuits_df1.circuitRef,circuits_df1.name,circuits_df1.location,circuits_df1.country,circuits_df1.lat,circuits_df1.lng,circuits_df1.alt)
+
+# COMMAND ----------
+
+display(circuits_selected_df2)
+
+# COMMAND ----------
+
+# 3rd way of selecting columns from a dataframe
+circuits_selected_df3 = circuits_df1.select(circuits_df1["circuitId"],circuits_df1["circuitRef"],circuits_df1["name"],circuits_df1["location"],circuits_df1["country"],circuits_df1["lat"],circuits_df1["lng"],circuits_df1["alt"])
+
+# COMMAND ----------
+
+circuits_selected_df3.show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+#using the 4th option which is nothing but with col
+circuits_selected_df4 = circuits_df1.select(col("circuitId") \
+    ,col("circuitRef"),col("name"),col("location"),col("country").alias("race_country")\
+    ,col("lat"),col("lng"),col("alt"))
+
+# COMMAND ----------
+
+circuits_selected_df4.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ### Rename the columns can be done in couple of ways
+# MAGIC 1) using an alias as showed above
+# MAGIC 2) using a specific api withColumnRenamed
+
+# COMMAND ----------
+
+circuits_df_renamed = circuits_selected_df4.withColumnRenamed("circuitId","circuit_Id")\
+    .withColumnRenamed("circuitRef","circuit_ref") \
+    .withColumnRenamed("lat","latitude") \
+    .withColumnRenamed("lng","longitude")\
+    .withColumnRenamed("alt","altitude")
+
+# COMMAND ----------
+
+display(circuits_df_renamed)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Adding the new columns 
+# MAGIC - Can be achieved through withColumn
+# MAGIC - Here i am adding an audit columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+circuits_final_df = circuits_df_renamed.withColumn("ingested_date",current_timestamp())
+
+# COMMAND ----------
+
+display(circuits_final_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - Use lit function let us say if we want a value as a column with the string/ value   (Just to know about the lit)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import lit
+
+circuits_final_df.withColumn("Env",lit("Production")).show()
+
+# COMMAND ----------
+
+
