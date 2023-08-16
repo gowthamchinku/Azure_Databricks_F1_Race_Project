18f8def2575bb9f08b2ba0a65cdf8608f2885e2c
gautamchinku@gmail.com <gautamchinku@gmail.com>
2023-08-16 04:09:06 +0000

    Commt

---
+++ formula1_project/includes/Common_functions.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+def add_ingestion_date(input_df):
+  output_df = input_df.withColumn("ingested_date",current_timestamp())
+  return output_df
+
+# COMMAND ----------
+
+
